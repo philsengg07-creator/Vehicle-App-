4e1ce431fef4155de176ee7374a26226615336d3
Phils <philsengg07@gmail.com>
2025-09-18 09:53:54 +0000

    cannot get notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
-// src/public/firebase-messaging-sw.js
-importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js");
+// public/firebase-messaging-sw.js
+importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js");
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdJXY7HLBRqWzur4JH3FNuVOCe_ItTyOk",
@@ -8,7 +8,8 @@ const firebaseConfig = {
   projectId: "studio-6451719734-ee0cd",
   storageBucket: "studio-6451719734-ee0cd.firebasestorage.app",
   messagingSenderId: "938204376421",
-  appId: "1:938204376421:web:fc26601d358695c130858c"
+  appId: "1:938204376421:web:fc26601d358695c130858c",
+  databaseURL: "https://studio-6451719734-ee0cd-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -20,7 +21,7 @@ messaging.onBackgroundMessage((payload) => {
     "[firebase-messaging-sw.js] Received background message ",
     payload
   );
-  
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
