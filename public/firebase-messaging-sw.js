73ffd50229c2ea10b423ec2ca662265c45bcb1a6
Phils <philsengg07@gmail.com>
2025-09-18 09:57:52 +0000

    not get notification where you send notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,15 @@
 // public/firebase-messaging-sw.js
-importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdJXY7HLBRqWzur4JH3FNuVOCe_ItTyOk",
-  authDomain: "studio-6451719734-ee0cd.firebaseapp.com",
-  projectId: "studio-6451719734-ee0cd",
-  storageBucket: "studio-6451719734-ee0cd.firebasestorage.app",
-  messagingSenderId: "938204376421",
-  appId: "1:938204376421:web:fc26601d358695c130858c",
-  databaseURL: "https://studio-6451719734-ee0cd-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    apiKey: "AIzaSyAdJXY7HLBRqWzur4JH3FNuVOCe_ItTyOk",
+    authDomain: "studio-6451719734-ee0cd.firebaseapp.com",
+    projectId: "studio-6451719734-ee0cd",
+    storageBucket: "studio-6451719734-ee0cd.firebasestorage.app",
+    messagingSenderId: "938204376421",
+    appId: "1:938204376421:web:fc26601d358695c130858c",
+    databaseURL: "https://studio-6451719734-ee0cd-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -17,15 +17,11 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "/firebase-logo.png",
+    icon: '/favicon.ico'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
