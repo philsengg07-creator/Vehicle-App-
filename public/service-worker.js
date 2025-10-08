c548e4c30605b5880be895ea38f07c2917266a81
Phils <philsengg07@gmail.com>
2025-10-08 18:58:20 +0000

    Interface with our API by sending an HTTP POST request to the following

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,3 @@
-// Import Pushy Service Worker 1.0.24
-importScripts('https://sdk.pushy.me/web/1.0.24/pushy-service-worker.js');
+// This script is required by Pushy to handle notifications in the background.
+// It must be placed in the root of your public directory.
+importScripts('https://sdk.pushy.me/service-worker.js?appId=668f7633e7e891392b67f185');
