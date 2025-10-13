686740d2ba3487a663f4ffbf465662b504836d4f
Phils <philsengg07@gmail.com>
2025-10-13 11:58:24 +0000

    use a service worker file again'

--- public/pushy-service-worker.js
+++ public/pushy-service-worker.js
@@ -1,1 +1,4 @@
+// This file is intentionally left blank.
+// It is used by the Pushy SDK to register a service worker.
+// The actual service worker logic is loaded from the Pushy CDN.
 importScripts('https://sdk.pushy.me/web/pushy-sw.js');
