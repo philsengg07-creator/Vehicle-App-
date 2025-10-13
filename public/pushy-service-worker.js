31aa2386a02e0d2af86593fe6938ce9f61fb4450
Phils <philsengg07@gmail.com>
2025-10-13 12:00:29 +0000

    Access to script at 'https://sdk.pushy.me/web/pushy-sdk.js' from origin

--- public/pushy-service-worker.js
+++ public/pushy-service-worker.js
@@ -1,4 +1,2 @@
-// This file is intentionally left blank.
-// It is used by the Pushy SDK to register a service worker.
-// The actual service worker logic is loaded from the Pushy CDN.
-importScripts('https://sdk.pushy.me/web/pushy-sw.js');
+// This file is intentionally left blank in the initial setup.
+// It will be updated with the correct importScripts line in a subsequent step.
